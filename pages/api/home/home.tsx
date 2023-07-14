4a0d212ce741c418c4c04a02a8ab2ddf1608a5cd
fredespi <fredrik.espinoza@gmail.com>
2023-07-14 11:33:39 +0200

    Updated the page title and meta description.

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -360,8 +360,8 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
-        <meta name="description" content="ChatGPT but better." />
+        <title>Silopedia Chat UI</title>
+        <meta name="description" content="Chat with Silopedia." />
         <meta
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
